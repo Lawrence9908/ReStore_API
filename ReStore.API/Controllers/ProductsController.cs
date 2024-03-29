7a4540dc7a0c00420100a1563948d3f6ef8997c8
Azwie@050317 <23014643@mvula.univen.ac.za>
2024-03-29 19:09:05 +0200

    Add project files.

---
+++ ReStore.API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReStore.API.Data;
+using ReStore.API.Entities;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ReStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        [HttpGet] //api/products/
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _storeContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:int}")] //api/products/3
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product  = await _storeContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+            return Ok(product);
+        }
+
+    }
+}
+
---
+++ ReStore.API/Data/StoreContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReStore.API.Entities;
+
+namespace ReStore.API.Data
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
